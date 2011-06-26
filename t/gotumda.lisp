0ea7eea8679da12b6685aae95bfe078f061e2618
fukamachi <e.arrows@gmail.com>
2011-06-26 10:42:10 +0900

    fixed unit tests.

--- t/gotumda.lisp
+++ t/gotumda.lisp
@@ -20,20 +20,7 @@
 (diag "Starting..")
 (gotumda:start :mode "test")
 
-(diag "index")
-(let ((index-page (http-request "http://localhost:4242/")))
-  (like index-page "Welcome to Gotumda!"
-        "index")
-  (is (http-request "http://localhost:4242/pc/")
-      index-page
-      "index (pc)"))
-
 (diag "all-tasks")
-(let ((tasks-page (http-request "http://localhost:4242/all-tasks")))
-  (is tasks-page "" "all-tasks")
-  (is (http-request "http://localhost:4242/pc/all-tasks")
-      tasks-page
-      "all-tasks (pc)"))
 (is (flex:octets-to-string
      (http-request "http://localhost:4242/api/all-tasks"))
     "[]"
@@ -41,19 +28,14 @@
 
 (diag "update")
 (http-request "http://localhost:4242/update"
-              :method :POST
-              :parameters '(("body" . "Buy a milk")))
+                  :method :POST
+                  :parameters '(("body" . "Buy a milk")))
 
 (diag "all-tasks 2")
-(let ((tasks-page (http-request "http://localhost:4242/all-tasks")))
-  (is tasks-page "" "all-tasks")
-  (is (http-request "http://localhost:4242/pc/all-tasks")
-      tasks-page
-      "all-tasks (pc)"))
-(is (flex:octets-to-string
-     (http-request "http://localhost:4242/api/all-tasks"))
-    "[]"
-    "all-tasks (api)")
+(like (flex:octets-to-string
+       (http-request "http://localhost:4242/api/all-tasks"))
+      "Buy a milk"
+      "all-tasks (api)")
 
 (diag "Stopping..")
 
